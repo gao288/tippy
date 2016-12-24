a30e911b76e386dbea5d15f9c4f5b3fc3af230f6
Xufei Gao <xufei@Xufeis-iMac.local>
2016-12-24 05:36:25 -0500

    Here is my commit message

---
+++ tipCal/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tipCal
+//
+//  Created by Xufei Gao on 12/22/16.
+//  Copyright © 2016 Xufei Gao. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}
